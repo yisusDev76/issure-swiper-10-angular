bfef6552379308edfdf8313399cc72ee64d7d5fb
yisusDev76 <je.parra.navarrete@gmail.com>
2023-07-27 19:12:52 -0600

    add swiper and buttons for replicate error

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -6,7 +6,7 @@ import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
-
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 @NgModule({
   imports: [
     IonicModule,
@@ -15,6 +15,7 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     ExploreContainerComponentModule,
     Tab1PageRoutingModule
   ],
-  declarations: [Tab1Page]
+  declarations: [Tab1Page],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class Tab1PageModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,29 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  @ViewChild('swiper')
+  swiperRef: ElementRef | undefined;
+  swiper?:Swiper
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {}
+
+  swiperReady(){
+    this.swiper = this.swiperRef?.nativeElement.swiper;
+  }
+
+  swiperSlideChanged(e: any) {
+    console.log('changed: ', e);
+  }
+
+  next() {
+    this.swiper?.slideNext();//Thie line broken code
+  }
+
+
+  async start() {
+    console.log("go to login");
+  }
 
 }
