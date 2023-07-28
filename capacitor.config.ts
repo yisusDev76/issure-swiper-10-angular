6f345389f4858aa5375cd9b2a6d2714e27f4f9f5
Jesús Parra <chuchin1770@gmail.com>
2023-07-28 00:59:52 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'issure-swiper10-angular',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
